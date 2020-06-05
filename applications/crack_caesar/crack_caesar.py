9c15c685892cd6861bf241a2c7742a3d693d57d1
Jacob Prudent <jake.prudent@gmail.com>
2020-06-05 00:11:24 -0700

    ceasar_cypher finished

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -2,4 +2,42 @@
 # decode it.
 
 # Your code here
+test = 1
 
+with open("ciphertext.txt") as f:
+    words = f.read()
+
+letter_feq_dict = {}
+
+tranlation_dict = {}
+
+charlist = []
+ordered_letter_list = ['E', 'T', 'A', 'O', 'H', 'N', 'R', 'I', 'S', 'D', 'L', 'W', 'U',
+                       'G', 'F', 'B', 'M', 'Y', 'C', 'P', 'K', 'V', 'Q', 'J', 'X', 'Z']
+
+for char in words:
+    if char.isalpha() and char != 'â':
+        if char not in letter_feq_dict:
+            letter_feq_dict[char] = 1
+        else:
+            letter_feq_dict[char] += 1
+# print(letter_feq_dict)
+for key in letter_feq_dict:
+    charlist.append((letter_feq_dict[key], key))
+
+charlist.sort(reverse=True)
+print(charlist)
+currentIndex = 0
+
+for letter in ordered_letter_list:
+    tranlation_dict[charlist[currentIndex][1]] = letter
+    currentIndex += 1
+print(tranlation_dict)
+translation = ""
+for char in words:
+    if char in tranlation_dict:
+        translation += tranlation_dict[char]
+    else:
+        translation += char
+
+print(translation)
